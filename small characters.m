6765dcab9fdbe13ad70266d97d4ed340bcb622b2
DeviSri2411 <157139928+DeviSri2411@users.noreply.github.com>
2024-07-17 21:42:56 +0530

    Update and rename small_char.m to small characters.m
    Handwritten Lowercase English Character Recognition using CNN
    Overview
    
    
    It contains a MATLAB implementation of a Convolutional Neural Network (CNN) for recognizing lowercase handwritten English characters. The model achieves a recognition accuracy of 88.97%, with precision, recall, and F1-score metrics all at 0.88974.
    
    Project Details
    Model Type: Convolutional Neural Network (CNN)
    Dataset: Handwritten lowercase English characters
    Accuracy: 88.97%
    Precision: 0.88974
    Recall: 0.88974
    F1-Score: 0.88974

--- small characters.m
+++ small characters.m
@@ -13,26 +13,19 @@ out1=predict(net,imdsTest)
 accuracy=sum(out==imdsTest.Labels)/numel(imdsTest.Labels)
 plotconfusion(out,imdsTest.Labels)
 co = confusionmat(imdsTest.Labels, out)
-
 % Calculate precision
 precision = diag(co) ./ sum(co, 1)';
-
 % Calculate recall
 recall = diag(co) ./ sum(co, 2);
-
 % Calculate F1-score
 f1_score = 2 * (precision .* recall) ./ (precision + recall);
-
-
 % Display precision, recall, and F1-score
 disp(['Precision: ', num2str(precision')])
 disp(['Recall: ', num2str(recall')])
 disp(['F1-score: ', num2str(f1_score')])
-
 overall_precision = sum(diag(co)) / sum(co(:));
 overall_recall = sum(diag(co)) / sum(co(:));
 overall_f1_score = 2 * (overall_precision * overall_recall) / (overall_precision + overall_recall);
-
 % Display overall precision, recall, and F1-score
 disp(['Overall Precision: ', num2str(overall_precision)]);
 disp(['Overall Recall: ', num2str(overall_recall)]);
